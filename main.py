4b9c939ce49e43787456168a5d6ff113a84480af
Atharva Chitale <atharvachitale2005@gmail.com>
2023-07-19 22:04:26 +0530

    main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import wikipedia
 import platform
 import pyaudio
 import pyjokes
+import pyowm
+import webbrowser
 
 
 engine = pyttsx3.init('dummy','sapi5')
@@ -78,6 +80,30 @@ def open_app(app_name):
     elif system == "linux":
         os.system("xdg-open " + app_name)
 
+def get_weather():
+    owm = pyowm.OWM('your_openweathermap_api_key')
+    observation = owm.weather_at_place('City, Country')  # Replace 'City, Country' with your desired location
+    weather = observation.get_weather()
+    temperature = weather.get_temperature('celsius')['temp']
+    status = weather.get_status()
+    return f"The weather in City is {status.lower()} with a temperature of {temperature}°C."
+
+def get_date():
+    now = datetime.datetime.now()
+    return now.strftime("Today is %A, %d %B %Y.")
+
+def get_random_fact():
+    facts = [
+        "The Eiffel Tower can grow up to 6 inches taller during the summer.",
+        "Polar bears have black skin under their white fur.",
+        "The strongest muscle in the human body is the tongue.",
+        "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly safe to eat.",
+        "Octopuses have three hearts. Two pump blood through the gills, and one pumps blood through the rest of the body.",
+        "A group of flamingos is called a 'flamboyance.'",
+        "The fingerprints of a koala are so indistinguishable from humans that they have, on occasion, been confused at a crime scene.",
+        "Bananas are berries, but strawberries are not.",
+    ]
+
 if __name__ == "__main__":
     wishMe()
     while True:
@@ -95,6 +121,21 @@ if __name__ == "__main__":
         elif "open" in query:
             app_name = query.split("open ")[-1]
             open_app(app_name)
+        elif "tell me a joke" in query:
+            joke = pyjokes.get_joke()
+            speak(joke)
+        elif "weather" in query:
+            weather_info = get_weather()
+            speak(weather_info)
+        elif "date" in query:
+            date_info = get_date()
+            speak(date_info)
+        elif "random fact" in query:
+            fact = get_random_fact()
+            speak(fact)
+        elif "open website" in query:
+            speak("Sure, which website would you like to open?")
+            website = takeCommand()
+            webbrowser.open(f"https://{website}")
         elif "quit" in query or "exit" in query:
             speak("Goodbye!")
-        
