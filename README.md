865579e56e69d7be0585285a61f2baff50e78f3a
atharvachitale765 <atharvachitale2005@gmail.com>
2024-01-12 21:09:51 +0530

    update

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # max-bot-
 a simple bot that performs your basic tasks.
  
-For Installation:
+Setup (Windows, Linux, MacOS):
 ```
 git clone https://atharvachitale765/max-bot-.git
 cd max-bot
+pip install -r requirements.txt
 ```
-Run:
+Run (Winodws, Linux and MacOS) :
 ```
 python main.py
+```
+Linux Additional Dependencies 
+```
+sudo apt upgrade
+sudo apt install espeak && sudo apt install ffmpeg
 ```
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import subprocess
 import pyttsx3
 import datetime
 import speech_recognition as sr
 import os
+import sys
+from sys import platform
 import wikipedia
 import platform
 import pyjokes
@@ -10,6 +13,7 @@ import webbrowser
 import random
 import json
 import requests
+import urllib.parse
 
 engine = pyttsx3.init('espeak', 'sapi5')
 voices = engine.getProperty('voices')
@@ -35,25 +39,36 @@ def wish_me():
     else:
         speak("Good Evening!")
 
-    speak("Hello, I am Max! How should I call you?")
+    speak("Hello, I am Max! ,  How should I call you?")
     name = input("Enter Your name: ")
-    speak(f'Hello {name}! How can I help you today?')
+    speak(f'Hello {name}! , How can I help you today?')
 
 
 def take_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
+        print('Listening...')
         r.pause_threshold = 1
+        r.energy_threshold = 494
+        r.adjust_for_ambient_noise(source, duration=1)
+
         audio = r.listen(source)
 
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-us')
         print(f"User said: {query}\n")
+    except sr.UnknownValueError:
+        speak("Sorry, I didn't catch that. Could you please repeat?")
+        return "None"
     except Exception as e:
-        print("Error:", e)
-        print("Say that again, please...")
+        if 'ALSA lib' in str(e):
+            # Supresses unnecssary Linux ALSA Errors.
+            subprocess.call(["python3", "main.py"],
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            print("Error:", e)
+        speak("Say that again, please...")
         return "None"
     return query.lower()
 
@@ -73,12 +88,17 @@ def search_wikipedia(query):
 def open_app(app_name):
     system = platform.system().lower()
 
-    if system == "windows":
-        os.startfile(app_name)
-    elif system == "darwin":  # macOS
-        os.system("open -a " + app_name)
-    elif system == "linux":
-        os.system("xdg-open " + app_name)
+    try:
+        if system == "windows":
+            subprocess.Popen(["start", " ", app_name], shell=True)
+        elif system == "darwin":  # macOS
+            subprocess.Popen(["open", "-a", app_name])
+        elif system == "linux":
+            subprocess.Popen(["xdg-open", app_name])
+        else:
+            print(f"Unsupported operating system: {system}")
+    except Exception as e:
+        print("Error opening the application:", e)
 
 
 def get_weather():
@@ -112,6 +132,37 @@ def get_random_fact():
     return random.choice(facts)
 
 
+def play_youtube(query):
+    search_query = urllib.parse.urlencode({'search_query': query})
+    search_url = f"https://www.youtube.com/results?{search_query}"
+
+    try:
+        response = requests.get(search_url)
+        if response.status_code == 200:
+            videos = []
+            results = response.text.split(
+                '{"commandMetadata":{"webCommandMetadata":{"url":"/watch?v=')
+            for result in results[1:]:
+                video_id = result.split('"')[0]
+                title = result.split('title":{"runs":[{"text":"')[
+                    1].split('"')[0]
+                videos.append({'title': title, 'video_id': video_id})
+
+            # Assume the first result is the desired video
+            if videos:
+                first_video = videos[0]
+                video_url = f"https://www.youtube.com/watch?v={first_video['video_id']}"
+                speak(f"Playing video: {first_video['title']}")
+                webbrowser.open(video_url)
+            else:
+                speak("Sorry, I couldn't find any relevant videos.")
+
+        else:
+            speak("Sorry, I couldn't fetch YouTube search results at the moment.")
+    except Exception as e:
+        speak(f"Error: {e}")
+
+
 def process_query(query):
     if query in queries_config["greetings"]:
         speak("How can I help you?")
@@ -141,15 +192,43 @@ def process_query(query):
         if "play video" in query:
             speak("Sure, what topic or channel would you like to watch?")
             video_query = take_command()
-            search_query = f"youtube.com/results?search_query={video_query.replace(' ', '+')}"
+            search_query = f"https://www.youtube.com/results?search_query={video_query.replace(' ', '+')}"
             speak(f"Playing videos related to {video_query} on YouTube.")
             webbrowser.open(search_query)
+    elif "search google for" in query:
+        search_query = query.replace("search Google for", "").strip()
+        search_url = f"https://www.google.com/search?q={search_query.replace(' ', '+')}"
+        speak(f"Searching Google for {search_query}")
+        webbrowser.open(search_url)
+
+    elif query.startswith("play") and "on youtube" in query:
+        topic = query.split("play")[-1].replace("on youtube", "").strip()
+        topic = f"youtube.com/results?search_query={topic.replace(' ', '+')}"
+        speak(f"Playing videos related to {topic} on YouTube.")
+        webbrowser.open(topic)
+
     else:
         speak("Sorry, I don't understand that command.")
+
+## Only for Surpassing ASLA unwanted messages on Linux.##
+
+
+def ignoreStderr():
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    old_stderr = os.dup(2)
+    sys.stderr.flush()
+    os.dup2(devnull, 2)
+    os.close(devnull)
+    try:
+        yield
+    finally:
+        os.dup2(old_stderr, 2)
+        os.close(old_stderr)
 
 
 if __name__ == "__main__":
     wish_me()
+    take_command()
 
     while True:
         query = take_command()
